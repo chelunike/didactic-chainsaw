ccf9d9f01f9b91f1b25ca7169f1caa83054f6e9c
chelunike <chelunike@gmail.com>
2023-12-13 19:58:01 +0100

    Creación de proyecto gradle, moviendo las entidades y creando los ficheros de configuración

---
+++ src/test/java/com/didactic/chainsaw/ChainsawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didactic.chainsaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainsawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
